e1163c78035ed14e5f6fe4909c767b5f668e42e5
jamesire <j.j.reidy@gmail.com>
2024-09-18 00:33:02 +0100

    Added spotify player

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,6 +6,10 @@ const Home: React.FC = () => {
   return (
     <div className='home'>
         <div className='video-container'>
+            <iframe style={{borderRadius: "12px", border: "none"}} src="https://open.spotify.com/embed/album/1SFDtUTWaK2wANp6ipg9Gz?utm_source=generator" width="100%" height="400" allowFullScreen={false} allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+            <p>Listen to Black Poison Morning</p>
+        </div>
+        <div className='video-container'>
             <div className="player-wrapper">
                 <ReactPlayer className='react-player'
                     url="https://www.youtube.com/watch?v=BwidOaKZJaY"
